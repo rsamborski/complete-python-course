108be287e215339672f5470c641c3c5c87c7d6ca
Remigiusz Samborski <rsamborski@gmail.com>
2020-05-22 23:29:26 +0200

    Completed the app

--- course_contents/3_first_milestone_project/milestone_1/incomplete_app.py
+++ course_contents/3_first_milestone_project/milestone_1/incomplete_app.py
@@ -1,39 +1,58 @@
-# Incomplete app!
+# Super TURBO Indestructible App !!!
 
-MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
 movies = []
 
 
-# You may want to create a function for this code
-title = input("Enter the movie title: ")
-director = input("Enter the movie director: ")
-year = input("Enter the movie release year: ")
+# Adding new movie
+def add_movie():
+    title = input("Enter the movie title: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the movie release year: ")
 
-movies.append({
-    'title': title,
-    'director': director,
-    'year': year
-})
-
-
-# Create other functions for:
-#   - listing movies
-#   - finding movies
+    movies.append({
+        'title': title,
+        'director': director,
+        'year': year
+    })
 
 
-# And another function here for the user menu
-selection = input(MENU_PROMPT)
-while selection != 'q':
-    if selection == "a":
-        pass
-    elif selection == "l":
-        pass
-    elif selection == "f":
-        pass
-    else:
-        print('Unknown command. Please try again.')
+# Printing out single movie details
+def print_movie(movie):
+    print(f"{movie['title']} by {movie['director']} from {movie['year']}")
 
+
+# Printing out list of all movies
+def print_movies():
+    for movie in movies:
+        print_movie(movie)
+
+
+# Finding movie by inputted attribute and search string
+def find_movie():
+    attribute = input("Enter the attribute by which you are looking for (title, director, year): ")
+    search_string = input("What do you search for: ")
+    for movie in movies:
+        if movie[attribute].lower() == search_string.lower():
+            print_movie(movie)
+
+# Display user menu and handle input
+def display_menu():
+    MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
     selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == "a":
+            add_movie()
+            pass
+        elif selection == "l":
+            print_movies()
+            pass
+        elif selection == "f":
+            find_movie()
+            pass
+        else:
+            print('Unknown command. Please try again.')
+
+        selection = input(MENU_PROMPT)
 
 
-# Remember to run the user menu function at the end!
+display_menu()
