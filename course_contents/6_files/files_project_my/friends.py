07ef99c85068462c77d89cccbaaa145ad82c860c
Remigiusz Samborski <rsamborski@gmail.com>
2020-05-27 01:03:45 +0200

    Finished task: 84. Python Exercise: copying files

--- course_contents/6_files/files_project_my/friends.py
+++ course_contents/6_files/files_project_my/friends.py
@@ -1,6 +1,6 @@
 
 friends_list = input("Please give the name of your 3 friends: ").split(',')
-people_list = []
+#people_list = []
 
 with open("people.txt") as people_file:
     people_list = [l.strip() for l in people_file.readlines()]
@@ -9,6 +9,13 @@ with open("people.txt") as people_file:
 friends_set = set(friends_list)
 people_set = set(people_list)
 
+print(f"Friends: {friends_set}")
+print(f"People: {people_set}")
+
 nearby_friends_set = friends_set.intersection(people_set)
 
-with open("nearby_friends.txt", )
+print(f"Nearby friends: {nearby_friends_set}")
+
+with open("nearby_friends.txt", "w") as nearby_friends_file:
+    nearby_friends_file.write('\n'.join(nearby_friends_set))
+    nearby_friends_file.close()
