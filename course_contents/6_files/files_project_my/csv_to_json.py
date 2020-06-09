fede03df7a49f892b2f5fbc5e6d2c002041dfcbb
Remigiusz Samborski <rsamborski@gmail.com>
2020-06-09 23:06:04 +0200

    Finished task: 88. Exercise: a CSV to JSON converter

---
+++ course_contents/6_files/files_project_my/csv_to_json.py
@@ -0,0 +1,18 @@
+# Please read the instructions carefully and write your script here:
+# You need to:
+# - read data from csv_file.txt
+# - process data and convert them into a single JSON object
+# - store the JSON object into json_file.txt
+# Your code starts here:
+
+import json
+
+json_data = []
+
+with open("csv_file.txt", "r") as csv_file:
+    for line in csv_file:
+        (club, city, country) = line.strip().split(",")
+        json_data.append({"club": club, "country": country, "city": city})
+
+with open("json_file.txt", "w") as json_file:
+    json.dump(json_data, json_file)
